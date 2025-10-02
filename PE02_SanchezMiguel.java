bbbe1bde6258e0e6ab2fc9b8d7b1436e0bc0fd0b
Miguel Sanchez Cuadros <Mikikanet@gmail.com>
2025-10-02 19:59:47 +0200

    Finalitzacio de la primera decisio i la segona decisio

--- PE02_SanchezMiguel.java
+++ PE02_SanchezMiguel.java
@@ -26,7 +26,7 @@ public class PE02_SanchezMiguel {
             System.out.println("(2.) Explorar el laboratori cientific");
             int decisio1 = k.nextInt();
 
-                        if (decisio1 == 1) {
+            if (decisio1 == 1) {
                 System.out.println("El passsadís es bifurca en dues direccions.");
                 System.out.println("A l'esquerra, una porta blindada mig oberta deixa escapaar un so metàl·lic ritmic.");
                 System.out.println("A la dreta, un passadis llarg i fosc sembla perdre's en la foscor.");
@@ -51,7 +51,42 @@ public class PE02_SanchezMiguel {
                     } else {
                         System.out.println("(FINAL NEUTRE): Veus que hi ha una nau que funciona, marxes amb el misteri sense resoldre.");
                     }
+
+                } else {
+                    System.out.println("El passadis és estret i el terra grinyola sota els teus peus.");
+                    System.out.println("De sobte, darrere teu, un so de passades es fa cada vegada més intens.");
+                    System.out.println("Que fas?");
+                    System.out.println("(1.) Correr cap a l'escotilla");
+                    System.out.println("(2.) Amagar-te entre les cantonades.");
+                    int decisio3 = k.nextInt();
+
+                     if (decisio3 == 1 && teCasc) {
+                    System.out.println("(FINAL MISTERIÓS): Trobes una càpsula d'escapament i fuges amb el codi que tenia el casc.");
+                    } else if (decisio3 == 1 && teCasc) {
+                    System.out.println("(FINAL DESESPERANÇADOR): La càpsula no s'activa i l'alien et mata.");
+                    } else {
+                    System.out.println("(FINAL TRÀGIC): Sents els passos d'una “cosa misteriosa” aproparse. Una criatura alienígena et captura.");
+                    }
+
+                }
+            
+            } else {
+                System.out.println("El laboratori es ple de tubs de criogenesi trencat i taules cobertes de pols.");
+                System.out.println("Un lleu so prové d'un dels tubs encara actius.");
+                System.out.println("Al costat hi ha un ordinador central que sembla funcionar amb energia d'emergència.");
+                System.out.println("Que vols fer?");
+                System.out.println("(1.) Obrir un tub.");
+                System.out.println("Inspeccionar l'ordinador central.");
+                int decisio2 = k.nextInt();
+
+                if (decisio2 == 1 && teCasc) {
+                    System.out.println("(FINAL ESPERANÇADOR): Despertes un científic que et guia cap a la sortida.");
+                } else if ( decisio2 == 1 && teCasc == false) {
+                    System.out.println("(FINAL ESPANTÓS): L'experiment alliberat et devora.");
+                } else {
+                    System.out.println("(FINAL FANTASIOS): Descobreixes que hi ha un portal a la següent sala i escapes, pero no saps a on...");
                 }
             }
-    }
+            k.close();
+        }
 }
